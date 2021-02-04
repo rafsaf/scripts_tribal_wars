fa45112185be6cb6491c0efbd47ae81e4b8e4317
Rafał Safin <rafal.safin12@gmail.com>
2021-02-04 09:57:13 +0100

    added all scripts collect troops

---
+++ collect_troops_toolbar.js
@@ -0,0 +1,13 @@
+javascript: var Data = {
+    cache: true,
+    cacheTime: 30,
+    removedPlayers: "",
+    firstLineTroops: "Wioska,pik,miecz,topór,itp...,",
+    showFirstLineTroops: false,
+    showNicknamesTroops: false,
+    firstLineDeff: "Wioska,miejsce,pik,miecz,topór,itp...,",
+    showFirstLineDeff: false,
+    showNicknamesDeff: false,
+};
+$.getScript("https://xxx-jakis-tam-link-xxx");
+void 0;
